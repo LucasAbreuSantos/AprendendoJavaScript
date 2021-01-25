29ef9b28ecea7f65f0e9e1be95e71c8b6092df9d
Lucas de Abreu dos Santos <lukas.abreu1056@gmail.com>
2021-01-25 13:40:54 -0300

    update

--- Aula12Ex/script.js
+++ Aula12Ex/script.js
@@ -1,1 +1,6 @@
-alert('Olá')
+var dark = window.document.getElementById('DarkMode')
+dark.addEventListener('click',AlterarModo)
+function AlterarModo(){
+    window.document.body.style.background = 'black'
+    window.document.body.style.color = 'white'
+}
